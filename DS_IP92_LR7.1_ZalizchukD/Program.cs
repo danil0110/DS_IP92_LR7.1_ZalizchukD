056dff89ae84c289ecbc2168e74822297033f5b0
danil0110 <danilmail0110@gmail.com>
2020-04-20 15:10:35 +0300

    Added UI sketch

--- DS_IP92_LR7.1_ZalizchukD/Program.cs
+++ DS_IP92_LR7.1_ZalizchukD/Program.cs
@@ -6,7 +6,22 @@ namespace DS_IP92_LR7._1_ZalizchukD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = "graph.txt", prufer = "prufer.txt", choice;
+            Console.WriteLine("1. Дерево в код Прюфера\n2. Код Прюфера в дерево");
+            choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                
+            }
+            else if (choice == "2")
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: неверный ввод.");
+                Environment.Exit(0);
+            }
         }
     }
 }
