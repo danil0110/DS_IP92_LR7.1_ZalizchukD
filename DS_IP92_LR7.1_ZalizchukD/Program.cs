f9765ae0236de4a60248e87b2dfa60fcc7a6e387
danil0110 <danilmail0110@gmail.com>
2020-04-20 15:27:19 +0300

    Added class Graph and input constructor

--- DS_IP92_LR7.1_ZalizchukD/Program.cs
+++ DS_IP92_LR7.1_ZalizchukD/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace DS_IP92_LR7._1_ZalizchukD
 {
@@ -11,11 +13,11 @@ namespace DS_IP92_LR7._1_ZalizchukD
             choice = Console.ReadLine();
             if (choice == "1")
             {
-                
+                Graph graph = new Graph(input);
             }
             else if (choice == "2")
             {
-                
+                Graph graph = new Graph(prufer);
             }
             else
             {
@@ -24,4 +26,56 @@ namespace DS_IP92_LR7._1_ZalizchukD
             }
         }
     }
+
+    class Graph
+    {
+        private int n, m;
+        private int[,] mSmezh;
+        private int[] vertexPowers;
+        private List<int> pruferCode = new List<int>(), vertices;
+
+        public Graph(string path)
+        {
+            StreamReader sr = new StreamReader(path);
+
+            if (path == "graph.txt")
+            {
+                string read = sr.ReadLine();
+                string[] temp = read.Split(' ');
+                n = Convert.ToInt32(temp[0]);
+                m = Convert.ToInt32(temp[1]);
+                mSmezh = new int[n, n];
+                vertexPowers = new int[n];
+
+                for (int i = 0; i < m; i++)
+                {
+                    read = sr.ReadLine();
+                    temp = read.Split(' ');
+                    int a = Convert.ToInt32(temp[0]) - 1, b = Convert.ToInt32(temp[1]) - 1;
+                    mSmezh[a, b] = 1;
+                    mSmezh[b, a] = 1;
+                }
+
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                        if (mSmezh[i, j] == 1)
+                            vertexPowers[i]++;
+            }
+            else if (path == "prufer.txt")
+            {
+                string read = sr.ReadLine();
+                string[] temp = read.Split(' ');
+                foreach (var el in temp)
+                    pruferCode.Add(Convert.ToInt32(el) - 1);
+
+                n = pruferCode.Count + 2;
+                mSmezh = new int[n, n];
+                vertices = new List<int>();
+                for (int i = 0; i < n; i++)
+                    vertices.Add(i);
+                
+            }
+        }
+    }
+    
 }
